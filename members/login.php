43c5fa73fbc80319e51659050a31ecdeb03fed20
Tripleo Nova <you@youfigureitout.com>
2022-12-18 00:32:18 -0500

    Initial commit

---
+++ members/login.php
@@ -0,0 +1,37 @@
+
+<p>
+<table border="1" width="100%">
+<tr>
+  <td align="center" width="50%">
+
+  Sign in to edit your member info:
+  <form action="http://tunes.org/members/edit.php" method="POST">
+  <table border="0">
+    <tr>
+      <td>Alias (case sensitive):</td>
+      <td><input type="text" name="alias"></td>
+    </tr>
+    <tr>
+      <td>Password:</td>
+      <td><input type="password" name="password"></td>
+    </tr>
+  </table>
+  <input type="submit" value="Edit My Member Info">
+  </form>
+
+  </td>
+
+  <td align="center" width="50%">
+
+  Forgot password?
+  <form action="http://tunes.org/members/mailpw.php" method="POST">
+  <table border="0">
+  <tr>
+    <td>Alias (case sensitive):</td>
+    <td><input type="text" name="alias"></td>
+  </tr>
+  </table>
+  <input type="submit" value="Mail Password">
+  </form>
+
+</table>
